0cb531cb2048fe2b27ab900f875fadd7739d1cc3
r4coder <ritheeshk2003@gmail.com>
2025-12-05 16:26:31 +0530

    Update 06.Generate all binary strings.cpp

--- recursion/06.Generate all binary strings.cpp
+++ recursion/06.Generate all binary strings.cpp
@@ -1,1 +1,35 @@
-
+class Solution{
+    public:
+    void generate(int n,string curr,vector<string>&ans)
+    {
+        if(curr.size()==n)
+        {
+            ans.push_back(curr);
+            return;
+        }
+        
+        generate(n,curr+"0",ans);
+        
+        if(curr.empty()||curr.back()!=1)
+        {
+            generate(n,curr+"1",ans);
+            
+        }
+    }
+        
+        
+        
+        
+        
+    }
+    
+    
+    vector<string> validStrings(int n)
+    {
+        vector<string>ans;
+        generate(n,"",ans);
+        return ans;
+        
+    }
+      
+};
