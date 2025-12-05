b498c0cea5477a822e3bd14f3cbe09f6b741a687
r4coder <ritheeshk2003@gmail.com>
2025-12-06 02:02:06 +0530

    Add function to generate all subsets of integers
    Implement a function to generate all subsets of a given vector of integers using bit manipulation.

--- recursion/09.Print all Subsequences.cpp
+++ recursion/09.Print all Subsequences.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+ 
+     int n=nums.size();
+     int total=1<<n;
+     vector<vector<int>>f;
+     vector<int>sub;
 
+     for(int mask=0;mask<total;mask++)
+     {
+         vector<int>sub;
+
+         for(int i=0;i<n;i++)
+         {
+            if(mask&(1<<i))
+            {
+             sub.push_back(nums[i]);
+            }
+         }
+       
+
+        f.push_back(sub);
+
+
+     }
+
+     return f;   
+    }
+};
