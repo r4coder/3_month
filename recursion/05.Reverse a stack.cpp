b903d3c261e5fc69683bc1b28f43bca40a270262
r4coder <ritheeshk2003@gmail.com>
2025-12-05 13:03:34 +0530

    Refactor stack reversal and add insert function
    Refactor the reverse function to use O(1) auxiliary space and add an insertB function for inserting elements at the bottom of the stack.

--- recursion/05.Reverse a stack.cpp
+++ recursion/05.Reverse a stack.cpp
@@ -1,1 +1,70 @@
+//O(n) auxillary space
+class Solution{
+    public:
+    
+      void reverse(stack<int>&st)
+      {
+          if(st.empty())
+          {
+              return;
+          }
+          
+          int top=st.top();
+          st.pop();
+          
+          reverse(st);
+          
+          stack<int>temp;
+          while(!st.empty())
+          {
+              temp.push(st.top());
+              st.pop();
+          }
+          st.push(top);
+          
+          while(!temp.empty())
+          {
+              st.push(temp.top());
+              temp.pop();
+          }
+          
+          
+      }
+};
+//O(1) auxillary space
+class Solution{
+    public:
+    
+      void insertB(stack<int>&st,int y)
+      {
+          if(st.empty())
+          {
+              st.push(y);
+              return;
+          }
+          
+         int top=st.top();
+         st.pop();
+         insertB(st,y);
+         st.push(top);
+          
+          
+          
+          void insertB(stack<int>&st)
+          {
+              if(st.empty())
+              {
+                  return;
+              }
+              int top=st.top();
+              st.pop();
+              
+              reverse(st);
+              insertB(st,top);
+              
+          }
+          
+          
+      }
+};
 
