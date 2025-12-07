839b22bd50666ee3d1af1aceb06cc6867b95cb08
r4coder <ritheeshk2003@gmail.com>
2025-12-08 03:19:34 +0530

    Add function to check subsequence sum k
    Implement recursive solution to check for subsequence with sum k.

--- recursion/11.Check if there exists a subsequence with sum k.cpp
+++ recursion/11.Check if there exists a subsequence with sum k.cpp
@@ -1,1 +1,33 @@
+class Solution{
+    public:
+    
+bool solve(int i,vector<int>& nums,int sum,int k)
+        {
+              if(i>nums.size())
+              {
+                 return (sum==k);     
+              }
+              
+              if(take)
+              {
+                  return true;
+              }
+              
+              int take=solve(i+1,nums,sum+nums[i],k);
+              int n_take=solve(i+1,nums,sum,k);
+              
+              return take||n_take;
+            
+        }
+     
+    
+    
+    
+    
+    bool countSubsequenceWithTargetSum(vector<int>& nums, int k){
+        return solve(0,nums,0,k);
+    }
+};
+
+
 
