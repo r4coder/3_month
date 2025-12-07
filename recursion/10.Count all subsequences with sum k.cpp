d45fe6741cc52fa57b2764d6b0e6d8fb93e7e25c
r4coder <ritheeshk2003@gmail.com>
2025-12-08 02:52:07 +0530

    Update 10.Count all subsequences with sum k.cpp

--- recursion/10.Count all subsequences with sum k.cpp
+++ recursion/10.Count all subsequences with sum k.cpp
@@ -1,1 +1,27 @@
-
+class Solution{
+    public:
+    
+int solve(int i,vector<int>& nums,int sum,int k)
+        {
+              if(i>nums.size())
+              {
+                 return (sum==k)?1:0;   
+              }
+              
+              int take=solve(i+1,nums,sum+nums[i],k);
+              int n_take=solve(i+1,nums,sum,k);
+              
+              return take+n_take;
+            
+        }
+     
+    
+    
+    
+    
+    int countSubsequenceWithTargetSum(vector<int>& nums, int k){
+        
+         int n=nums.size();
+         return solve(0,nums,0,k);
+    }
+};
